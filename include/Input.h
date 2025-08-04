15f81f2916beebc37d5024906a7110523c3e365f
programmer-137 <120293861+programmer-137@users.noreply.github.com>
2025-08-04 14:01:18 -0700

    Added partial documentation and improved the KeyboardShortcut experience

--- include/Input.h
+++ include/Input.h
@@ -16,12 +16,20 @@
 
 namespace Syngine {
 
+/// @brief An actively processed keybind grouped with a label
+/// @section Input
 class InputBinding {
+public:
+	
 	// TODO: Fill in KeyboardKey
+	///
 	enum class KeyboardKey {
 		A, B, C // etc
 	};
 	
+	/// @brief A single non-directional modifier key
+	/// @section Input
+	/// @see ModifierKeys
 	enum class ModifierKey: uint8_t {
 		command = 1 << 0, gui = 1 << 0, windows = 1 << 0,
 		shift   = 1 << 1,
@@ -29,25 +37,54 @@ class InputBinding {
 		control = 1 << 3
 	};
 	
+	/// @brief A set of ``ModifierKey``s, mainly used in keyboard shortcuts
+	/// @section Input
+	/// @see ModifierKey
 	class ModifierKeys {
 	private:
 		const uint8_t rawValue;
 		
 	public:
 		ModifierKeys(uint8_t rawValue);
-		ModifierKeys(ModifierKey key);
-		// these will all take constexpr arrays
-		ModifierKeys(std::array<ModifierKey, 2>);
-		ModifierKeys(std::array<ModifierKey, 3>);
-		ModifierKeys(std::array<ModifierKey, 4>);
+		ModifierKeys(const ModifierKey key);
 		
-		constexpr ModifierKeys operator==(const ModifierKey& rhs);
+		/// @brief Returns true if the two ModifierKeys operators are equal
+		/// @param rhs The ModifierKeys instance to compare against
+		/// @return True when both instances are equal, false otherwise
+		constexpr bool operator==(const ModifierKey& rhs);
+		
+		/// @brief Returns the ModifierKeys instance equivilant to the right ModifierKey instance added to the left ModifierKeys instance
+		/// @param rhs The ModifierKey instance to combine
+		/// @return A ModifierKeys instance containing the logical OR of all the contained modifiers of both sides
+		constexpr InputBinding::ModifierKeys operator+(const InputBinding::ModifierKey& rhs);
+		
+		/// @brief Returns the ModifierKeys instance equivilant to the combination of both ModifierKeys instances
+		/// @param rhs The ModifierKeys instance to combine
+		/// @return A ModifierKeys instance containing the logical OR of all the contained modifiers of both sides
+		constexpr InputBinding::ModifierKeys operator+(const InputBinding::ModifierKeys& rhs);
 	};
 	
-	// TODO: Fill in KeyShortcut
-	class KeyShortcut {
+	// TODO: Fill in KeyboardShortcut
+	/// @brief A keyboard shortcut containing of a ``KeyboardKey`` and a set of ``ModifierKeys``
+	/// @section Input
+	class KeyboardShortcut {
+	private:
+		/// @brief The bound key
+		const KeyboardKey key;
+		
+		/// @brief The bound modifiers
+		const ModifierKeys modifiers;
+		
 	public:
-		KeyShortcut();
+		/// @brief Initialize a KeyboardShortcut from a key and a set of modifiers
+		KeyboardShortcut(KeyboardKey key, ModifierKeys modifiers);
+		
+		/// @brief Initialize a KeyboardShortcut from a key, with no modifiers set
+		KeyboardShortcut(KeyboardKey key);
+		
+		bool isPressed();
+		bool wasPressed();
+		bool wasReleased();
 	};
 	
 	// TODO: Fill in InputChord
@@ -59,11 +96,19 @@ class InputBinding {
 	constexpr InputBinding operator==(const InputBinding& rhs);
 	
 	InputBinding(KeyboardKey key);
-	InputBinding(KeyShortcut shortcut);
+	InputBinding(KeyboardShortcut shortcut);
 	InputBinding(InputChord chord);
 	
-	std::variant<KeyboardKey, ModifierKeys, KeyShortcut, InputChord> binding;
+private:
+	std::variant<KeyboardKey, ModifierKeys, KeyboardShortcut, InputChord> binding;
 };
+
+/// @brief Returns the ModifierKeys instance equivilant to the combination of both ModifierKey instances
+/// @param lhs A ModifierKey instance to combine
+/// @param rhs A ModifierKey instance to combine
+/// @return A ModifierKeys instance containing the logical OR of all the contained modifiers of both sides
+/// @section Input
+constexpr InputBinding::ModifierKeys operator+(const InputBinding::ModifierKey& lhs, const InputBinding::ModifierKey& rhs);
 
 class InputAction {
 public:
